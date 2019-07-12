ea48d2ba6d437ddd0bb672b2c270d97b3927d8da
Seiei Miyagi <hanachin@gmail.com>
2019-07-12 18:53:37 +0900

    Add single_cov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,7 @@
+require 'single_cov'
+SingleCov.setup :minitest
+SingleCov.covered!(file: 'plugins/redmine_importer/app/models/import_in_progress.rb')
+SingleCov.covered!(file: 'plugins/redmine_importer/app/helpers/importer_helper.rb')
+SingleCov.covered!(file: 'plugins/redmine_importer/app/controllers/importer_controller.rb')
+
 require File.expand_path('../../../../test/test_helper', __FILE__)
