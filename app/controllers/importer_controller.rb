agileware-jp/redmine_importer

commit 863956cf5636cc57bfa556ddad2251ca310fc8f3

fix(ImporterController): Fix occurring ArgumentError when same name custom field as standard field is exists (#19)

app/controllers/importer_controller.rb
@@ -55,13 +55,13 @@ class ImporterController < ApplicationController
     @attrs = []
     ISSUE_ATTRS.each do |attr|
       # @attrs.push([l_has_string?("field_#{attr}".to_sym) ? l("field_#{attr}".to_sym) : attr.to_s.humanize, attr])
-      @attrs.push([l_or_humanize(attr, prefix: 'field_'), attr])
+      @attrs.push([l_or_humanize(attr, prefix: 'field_'), "standard_field-#{attr}"])
     end
     @project.all_issue_custom_fields.each do |cfield|
-      @attrs.push([cfield.name, cfield.name])
+      @attrs.push([cfield.name, "custom_field-#{cfield.name}"])
     end
     IssueRelation::TYPES.each_pair do |rtype, rinfo|
-      @attrs.push([l_or_humanize(rinfo[:name]), rtype])
+      @attrs.push([l_or_humanize(rinfo[:name]), "issue_relation-#{rtype}"])
     end
     @attrs.sort!
   end
@@ -113,12 +113,12 @@ class ImporterController < ApplicationController
     if unique_attr.blank?
       if update_issue
         flash[:error] = l(:text_rmi_specify_unique_field_for_update)
-      elsif @attrs_map['parent_issue'].present?
+      elsif @attrs_map['standard_field-parent_issue'].present?
         flash[:error] = l(:text_rmi_specify_unique_field_for_column,
                           column: l(:field_parent_issue))
-      else IssueRelation::TYPES.each_key.any? { |t| @attrs_map[t].present? }
+      else IssueRelation::TYPES.each_key.any? { |t| @attrs_map["issue_relation-#{t}"].present? }
            IssueRelation::TYPES.each_key do |t|
-             if @attrs_map[t].present?
+             if @attrs_map["issue_relation-#{t}"].present?
                flash[:error] = l(:text_rmi_specify_unique_field_for_column,
                                  column: l("label_#{t}".to_sym))
              end
@@ -128,7 +128,7 @@ class ImporterController < ApplicationController
 
     # validate that the id attribute has been selected
     if use_issue_id
-      if @attrs_map['id'].blank?
+      if @attrs_map['standard_field-id'].blank?
         flash[:error] = 'You must specify a column mapping for id' \
           ' when importing using provided issue ids.'
       end
@@ -142,7 +142,7 @@ class ImporterController < ApplicationController
                 quote_char: iip.quote_char,
                 col_sep: iip.col_sep }
     CSV.new(iip.csv_data, csv_opt).each do |row|
-      project = Project.find_by_name(fetch('project', row))
+      project = Project.find_by_name(fetch('standard_field-project', row))
       project ||= @project
 
       begin
@@ -156,17 +156,17 @@ class ImporterController < ApplicationController
         issue = Issue.new
         issue.notify = false
 
-        issue.id = fetch('id', row) if use_issue_id
+        issue.id = fetch('standard_field-id', row) if use_issue_id
 
-        tracker = Tracker.find_by_name(fetch('tracker', row))
-        status = IssueStatus.find_by_name(fetch('status', row))
-        author = if @attrs_map.key?('author') && @attrs_map['author']
-                   user_for_login!(fetch('author', row))
+        tracker = Tracker.find_by_name(fetch('standard_field-tracker', row))
+        status = IssueStatus.find_by_name(fetch('standard_field-status', row))
+        author = if @attrs_map.key?('standard_field-author') && @attrs_map['standard_field-author']
+                   user_for_login!(fetch('standard_field-author', row))
                  else
                    User.current
                  end
-        priority = Enumeration.find_by_name(fetch('priority', row))
-        category_name = fetch('category', row)
+        priority = Enumeration.find_by_name(fetch('standard_field-priority', row))
+        category_name = fetch('standard_field-category', row)
         category = IssueCategory.find_by_project_id_and_name(project.id,
                                                              category_name)
 
@@ -178,21 +178,21 @@ class ImporterController < ApplicationController
           category.save
         end
 
-        if category.blank? && fetch('category', row).present?
+        if category.blank? && fetch('standard_field-category', row).present?
           @unfound_class = 'Category'
-          @unfound_key = fetch('category', row)
+          @unfound_key = fetch('standard_field-category', row)
           raise ActiveRecord::RecordNotFound
         end
 
-        if fetch('assigned_to', row).present?
-          assigned_to = user_for_login!(fetch('assigned_to', row))
+        if fetch('standard_field-assigned_to', row).present?
+          assigned_to = user_for_login!(fetch('standard_field-assigned_to', row))
           assigned_to = nil if assigned_to == User.anonymous
         else
           assigned_to = nil
         end
 
-        if fetch('fixed_version', row).present?
-          fixed_version_name = fetch('fixed_version', row)
+        if fetch('standard_field-fixed_version', row).present?
+          fixed_version_name = fetch('standard_field-fixed_version', row)
           fixed_version_id = version_id_for_name!(project,
                                                   fixed_version_name,
                                                   add_versions)
@@ -201,7 +201,7 @@ class ImporterController < ApplicationController
           fixed_version_id = nil
         end
 
-        watchers = fetch('watchers', row)
+        watchers = fetch('standard_field-watchers', row)
 
         issue.project_id = !project.nil? ? project.id : @project.id
         issue.tracker_id = !tracker.nil? ? tracker.id : default_tracker
@@ -264,10 +264,10 @@ class ImporterController < ApplicationController
         # Issue relations
         begin
           IssueRelation::TYPES.each_pair do |rtype, _rinfo|
-            next unless row[@attrs_map[rtype]]
+            next unless row[@attrs_map["issue_relation-#{rtype}"]]
 
             other_issue = issue_for_unique_attr(unique_attr,
-                                                row[@attrs_map[rtype]],
+                                                row[@attrs_map["issue_relation-#{rtype}"]],
                                                 row)
             relations = issue.relations.select do |r|
               (r.other_issue(issue).id == other_issue.id) \
@@ -398,20 +398,20 @@ class ImporterController < ApplicationController
       issue.priority_id = !priority.nil? ? priority.id : issue.priority_id
     end
     if assignable?(:subject)
-      issue.subject = fetch('subject', row) || issue.subject
+      issue.subject = fetch('standard_field-subject', row) || issue.subject
     end
     if assignable?(:tracker)
       issue.tracker_id = tracker.present? ? tracker.id : issue.tracker_id
     end
 
     # optional attributes
-    issue.description = fetch('description', row) if assignable?(:description)
+    issue.description = fetch('standard_field-description', row) if assignable?(:description)
     issue.category_id = category.try(:id) if assignable?(:category)
 
     %w[start_date due_date].each do |date_field_name|
       next unless assignable?(date_field_name)
 
-      date_field_value = fetch(date_field_name, row)
+      date_field_value = fetch("standard_field-#{date_field_name}", row)
 
       if date_field_value.present?
         begin
@@ -432,25 +432,25 @@ class ImporterController < ApplicationController
       end
     end
     issue.fixed_version_id = fixed_version_id if assignable?(:fixed_version)
-    issue.done_ratio = fetch('done_ratio', row) if assignable?(:done_ratio)
+    issue.done_ratio = fetch('standard_field-done_ratio', row) if assignable?(:done_ratio)
     if assignable?(:estimated_hours)
-      issue.estimated_hours = fetch('estimated_hours', row)
+      issue.estimated_hours = fetch('standard_field-estimated_hours', row)
     end
     if assignable?(:is_private)
-      issue.is_private = (convert_to_boolean(fetch('is_private', row)) || false)
+      issue.is_private = (convert_to_boolean(fetch('standard_field-is_private', row)) || false)
     end
   end
 
   def assignable?(field)
     raise unless ISSUE_ATTRS.include?(field.to_sym)
 
-    @attrs_map.key?(field.to_s)
+    @attrs_map.key?("standard_field-#{field}")
   end
 
   def handle_parent_issues(issue, row, ignore_non_exist, unique_attr)
     return unless assignable?(:parent_issue)
 
-    parent_value = fetch('parent_issue', row)
+    parent_value = fetch('standard_field-parent_issue', row)
     issue.parent_issue_id = if parent_value.present?
                               issue_for_unique_attr(unique_attr, parent_value, row).id
                             end
@@ -522,9 +522,9 @@ class ImporterController < ApplicationController
   def handle_custom_fields(add_versions, issue, project, row)
     custom_failed_count = 0
     issue.custom_field_values = issue.available_custom_fields.each_with_object({}) do |cf, h|
-      next h unless @attrs_map.key?(cf.name) # this cf is absent or ignored.
+      next h unless @attrs_map.key?("custom_field-#{cf.name}") # this cf is absent or ignored.
 
-      value = row[@attrs_map[cf.name]]
+      value = row[@attrs_map["custom_field-#{cf.name}"]]
       if cf.multiple
         h[cf.id] = process_multivalue_custom_field(project, add_versions, issue, cf, value)
       else
@@ -658,7 +658,7 @@ class ImporterController < ApplicationController
       return @issue_by_unique_attr[attr_value]
     end
 
-    if unique_attr == 'id'
+    if unique_attr == 'standard_field-id'
       issues = [Issue.find_by_id(attr_value)].compact
     else
       # Use IssueQuery class Redmine >= 2.3.0

app/helpers/importer_helper.rb
@@ -2,7 +2,7 @@ module ImporterHelper
   def matched_attrs(column)
     matched = ''
     @attrs.each do |k,v|
-      if v.to_s.casecmp(column.to_s.sub(" ") {|sp| "_" }) == 0 \
+      if v.to_s[/(?<=-).*/].casecmp(column.to_s.sub(" ") {|sp| "_" }) == 0 \
         || k.to_s.casecmp(column.to_s) == 0
 
         matched = v

test/functional/importer_controller_test.rb
@@ -125,7 +125,7 @@ class ImporterControllerTest < ActionController::TestCase
   test 'should handle issue relation' do
     other_issue = create_issue!(@project, @user, { subject: 'other_issue' })
     @iip.update!(csv_data: "#,Subject,Duplicated issue ID\n#{@issue.id},set other issue relation,#{other_issue.id}\n")
-    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['Duplicated issue ID'] = IssueRelation::TYPE_DUPLICATED }
+    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['Duplicated issue ID'] = "issue_relation-#{IssueRelation::TYPE_DUPLICATED}" }
     assert_response :success
     @issue.reload
     assert_equal 'set other issue relation', @issue.subject
@@ -138,7 +138,7 @@ class ImporterControllerTest < ActionController::TestCase
   test 'should error when assigned_to is missing' do
     @iip.update!(csv_data: "#,Subject,assigned_to\n#{@issue.id},barfooz,JohnDoe\n")
     @issue.update!(assigned_to: @user)
-    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['assigned_to'] = 'assigned_to' }
+    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['assigned_to'] = 'standard_field-assigned_to' }
     assert_response :success
     assert response.body.include?('Warning')
     @issue.reload
@@ -149,7 +149,7 @@ class ImporterControllerTest < ActionController::TestCase
   test 'should unset assigned_to when assigned_to user is not assignable' do
     User.create!(login: 'john', firstname: 'John', lastname: 'Doe', mail: 'john.doe@example.com')
     @iip.update!(csv_data: "#,Subject,assigned_to\n#{@issue.id},barfooz,john\n")
-    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['assigned_to'] = 'assigned_to' }
+    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['assigned_to'] = 'standard_field-assigned_to' }
     assert_response :success
     assert !response.body.include?('Warning')
     @issue.reload
@@ -164,7 +164,7 @@ class ImporterControllerTest < ActionController::TestCase
     @issue.custom_field_values.detect { |cfv| cfv.custom_field == assigned_by_field }.value = @user
     @iip.update!(csv_data: "#,Subject,assigned_by\n#{@issue.id},barfooz,JeanDoe\n")
     @issue.update!(assigned_to: @user)
-    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['assigned_by'] = 'assigned_by' }
+    post :result, params: build_params(update_issue: 'true').tap { |params| params[:fields_map]['assigned_by'] = 'standard_field-assigned_by' }
     assert_response :success
     assert response.body.include?('Warning')
     @issue.reload
@@ -175,7 +175,7 @@ class ImporterControllerTest < ActionController::TestCase
   test 'should not error when assigned_to is missing but use_anonymous is true' do
     @iip.update!(csv_data: "#,Subject,assigned_to\n#{@issue.id},barfooz,JohnDoe\n")
     @issue.update!(assigned_to: @user)
-    post :result, params: build_params(update_issue: 'true', use_anonymous: 'true').tap { |params| params[:fields_map]['assigned_to'] = 'assigned_to' }
+    post :result, params: build_params(update_issue: 'true', use_anonymous: 'true').tap { |params| params[:fields_map]['assigned_to'] = 'standard_field-assigned_to' }
     assert_response :success
     assert !response.body.include?('Warning')
     @issue.reload
@@ -190,7 +190,7 @@ class ImporterControllerTest < ActionController::TestCase
     @issue.custom_field_values.detect { |cfv| cfv.custom_field == assigned_by_field }.value = @user
     @iip.update!(csv_data: "#,Subject,assigned_by\n#{@issue.id},barfooz,JeanDoe\n")
     @issue.update!(assigned_to: @user)
-    post :result, params: build_params(update_issue: 'true', use_anonymous: 'true').tap { |params| params[:fields_map]['assigned_by'] = 'assigned_by' }
+    post :result, params: build_params(update_issue: 'true', use_anonymous: 'true').tap { |params| params[:fields_map]['assigned_by'] = 'custom_field-assigned_by' }
     assert_response :success
     assert !response.body.include?('Warning')
     @issue.reload
@@ -206,7 +206,7 @@ class ImporterControllerTest < ActionController::TestCase
     @issue.custom_field_values.detect { |cfv| cfv.custom_field == assigned_by_field }.value = @user
     @iip.update!(csv_data: "#,Subject,assigned_by\n#{@issue.id},barfooz,john\n")
     @issue.update!(assigned_to: @user)
-    post :result, params: build_params(update_issue: 'true', use_anonymous: 'true').tap { |params| params[:fields_map]['assigned_by'] = 'assigned_by' }
+    post :result, params: build_params(update_issue: 'true', use_anonymous: 'true').tap { |params| params[:fields_map]['assigned_by'] = 'custom_field-assigned_by' }
     assert_response :success
     assert !response.body.include?('Warning')
     @issue.reload
@@ -244,15 +244,15 @@ class ImporterControllerTest < ActionController::TestCase
       unique_field: '#',
       project_id: @project.id,
       fields_map: {
-        '#' => 'id',
-        'Subject' => 'subject',
-        'Tags' => 'Tags',
-        'Affected versions' => 'Affected versions',
-        'Priority' => 'priority',
-        'Tracker' => 'tracker',
-        'Status' => 'status',
-        'Watchers' => 'watchers',
-        'Area' => 'Area'
+        '#' => 'standard_field-id',
+        'Subject' => 'standard_field-subject',
+        'Tags' => 'custom_field-Tags',
+        'Affected versions' => 'custom_field-Affected versions',
+        'Priority' => 'standard_field-priority',
+        'Tracker' => 'standard_field-tracker',
+        'Status' => 'standard_field-status',
+        'Watchers' => 'standard_field-watchers',
+        'Area' => 'custom_field-Area'
       }
     )
   end

commit d2a20fd2fadb342c748c8f2d342c1b0e58a049de

Fix unique field lookup matching every open issue via invalid IssueQuery filter
- Map standard_field-id / standard_field-subject to the valid filter
  names issue_id / subject
- Strip the custom_field- prefix in translate_unique_attr so custom
  field unique keys resolve to cf_<id> again (broken since 863956c
  introduced the prefix)
- Raise UnsupportedUniqueField when add_filter rejects the filter name,
  and report it as a clear per-row error instead of silently searching
  all issues (tracker, priority etc. are intentionally unsupported as
  they would need name-to-id value translation)

refs #116671

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

app/controllers/importer_controller.rb
@@ -5,6 +5,7 @@ require 'tempfile'
 
 MultipleIssuesForUniqueValue = Class.new(RuntimeError)
 NoIssueForUniqueValue = Class.new(RuntimeError)
+UnsupportedUniqueField = Class.new(RuntimeError)
 
 class ImporterController < ApplicationController
   using RedmineImporter::Patches::Redmine51ToFsMethodPatch
@@ -305,6 +306,8 @@ class ImporterController < ApplicationController
             @deferred_callbacks.register(other_value, :add_relation, row[unique_field], rtype)
           rescue MultipleIssuesForUniqueValue
             @messages << "Warning: Multiple matches for relation target '#{other_value}'"
+          rescue UnsupportedUniqueField
+            @messages << l(:warning_unique_field_not_supported, attr: unique_field)
           end
         end
 
@@ -345,8 +348,10 @@ class ImporterController < ApplicationController
     # translate unique_attr if it's a custom field -- only on the first issue
     unless unique_attr_checked
       if unique_field && !ISSUE_ATTRS.include?(unique_attr.to_sym)
+        # fields_map values carry a 'custom_field-' prefix (see #match)
+        cf_name = unique_attr.delete_prefix('custom_field-')
         issue.available_custom_fields.each do |cf|
-          if cf.name == unique_attr
+          if cf.name == cf_name
             unique_attr = "cf_#{cf.id}"
             break
           end
@@ -396,6 +401,9 @@ class ImporterController < ApplicationController
         log_failure(row,
                     l(:warning_multiple_matches_for_update, issue_num: @failed_count + 1,
                                                             value: row[unique_field]))
+        raise RowFailed
+      rescue UnsupportedUniqueField
+        log_failure(row, l(:warning_unique_field_not_supported, attr: unique_field))
         raise RowFailed
       end
     end
@@ -497,6 +505,9 @@ class ImporterController < ApplicationController
     @failed_count += 1
     @failed_issues[@failed_count] = row
     @messages << l(:warning_parent_multiple_matches, issue_num: @failed_count, value: parent_value)
+    raise RowFailed
+  rescue UnsupportedUniqueField
+    log_failure(row, l(:warning_unique_field_not_supported, attr: unique_field))
     raise RowFailed
   end
 
@@ -744,7 +755,11 @@ class ImporterController < ApplicationController
 
       query = query_class.new(name: '_importer', project: @project)
       query.add_filter('status_id', '*', [1])
-      query.add_filter(unique_attr, '=', [attr_value])
+      # add_filter silently ignores invalid filter names (returning nil), which
+      # would turn the query into an unfiltered "all issues" search
+      if query.add_filter(query_filter_for_unique_attr(unique_attr), '=', [attr_value]).nil?
+        raise UnsupportedUniqueField, "'#{unique_attr}' is not a valid IssueQuery filter"
+      end
 
       issues = Issue.joins([:project])
                     .includes(%i[assigned_to status tracker project priority
@@ -764,6 +779,18 @@ class ImporterController < ApplicationController
       raise NoIssueForUniqueValue, "No issue with #{unique_attr} of '#{attr_value}' found"
     else
       issues.first
+    end
+  end
+
+  # Maps a unique_attr from fields_map to a valid IssueQuery filter name.
+  # Custom fields are already translated to 'cf_<id>' by translate_unique_attr.
+  # Other standard fields (tracker, priority, ...) would require translating
+  # names to ids, so they are left as-is and rejected by the add_filter guard.
+  def query_filter_for_unique_attr(unique_attr)
+    case unique_attr
+    when 'standard_field-id' then 'issue_id'
+    when 'standard_field-subject' then 'subject'
+    else unique_attr
     end
   end
 

config/locales/en.yml
@@ -67,4 +67,5 @@ en:
   warning_watcher_not_found: "Warning: When trying to add watchers on issue %{issue_num} below, User %{login} was not found"
   warning_custom_field_invalid: "Warning: When trying to set custom field %{field_name} on issue %{issue_num} below, value %{value} was invalid"
   warning_duplicate_unique_value: "Warning: Unique field %{attr} with value '%{value}' in issue %{issue_num} has duplicate record"
+  warning_unique_field_not_supported: "Warning: The unique field %{attr} cannot be used to find existing issues. Use # (ID), Subject, or a custom field that is used as a filter."
 

config/locales/ja.yml
@@ -68,3 +68,4 @@ ja:
   warning_watcher_not_found: "警告: チケット %{issue_num} のウォッチャー追加時に、ユーザー %{login} が見つかりませんでした。"
   warning_custom_field_invalid: "警告: チケット %{issue_num} のカスタムフィールド %{field_name} に、値 %{value} は無効です。"
   warning_duplicate_unique_value: "警告: チケット %{issue_num} の %{attr} の値 '%{value}' はすでに存在します。"
+  warning_unique_field_not_supported: "警告: ユニークフィールド %{attr} ではチケットを検索できません。#（ID）、題名、または「フィルタとして使用」が有効なカスタムフィールドを指定してください。"